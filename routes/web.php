3e5f5e1871114ec29543ae63ae589c0a4ea92be1
Oliver <oliverparra1108@gmail.com>
2025-09-15 17:03:01 -0400

    Primer commit: Hola Mundo en Laravel

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HolaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hola',[HolaController::class,'index'] );
